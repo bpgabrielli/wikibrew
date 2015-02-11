ee3253d8de9ee5b9a41a9b3a7d898a4dc769d01c
bpgabrielli <bpgabrielli@gmail.com>
2015-02-10 20:41:41 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikibrew::Application.config.secret_key_base = '8aecc95b17bed6deaef295cd814fcf8d11f7232b5787d62a5447ee170a4547c9e803748d82af1c4f370d6cbd23be7f4e1cc8ce0b9928e55949d5db7a7a5323fd'
